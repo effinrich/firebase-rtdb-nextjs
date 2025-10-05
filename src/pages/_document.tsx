0a2f4a5adec312f027cf8ac3041aeafa35bf1407
Rich Tillman <richtillman@gmail.com>
2025-10-05 04:10:43 -0400

    cleanup:  just cleanup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <script
-          src="https://cdn.softgen.ai/script.js"
-          async
-          data-softgen-monitoring="true"
-        /> */}
       </Head>
       <body>
         <Main />
