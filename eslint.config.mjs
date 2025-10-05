7c8c4d8329f3fc18c32b64e92cdc1e93798ad8c7
Rich Tillman <richtillman@gmail.com>
2025-10-05 17:53:23 -0400

    feat: add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from '@eslint/eslintrc'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -13,16 +16,13 @@ const baseConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript')
 ]
 
-const eslintConfig = [
-  ...baseConfig,
-  {
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'warn'
-    }
+const eslintConfig = [...baseConfig, {
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn'
   }
-]
+}, ...storybook.configs["flat/recommended"]]
 
 export default eslintConfig
